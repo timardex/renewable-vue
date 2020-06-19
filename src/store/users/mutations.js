b672f9d599b880689d83565218b0c00a2a83ecbf
timardex <timardex@gmail.com>
2020-06-19 12:25:17 +0200

    fix sorting order

--- src/store/users/mutations.js
+++ src/store/users/mutations.js
@@ -12,9 +12,9 @@ export const mutations = {
   SORT_BY(state, payload) {
     state.usersList = state.usersList.sort((a, b) => {
       if(payload.order) {
-        return a[payload.sort] < b[payload.sort] ? -1 : 1
+        return a[payload.sort].toLowerCase().split(' ') < b[payload.sort].toLowerCase().split(' ') ? -1 : 1
       } 
-      return a[payload.sort] > b[payload.sort] ? -1 : 1
+      return a[payload.sort].toLowerCase().split(' ') > b[payload.sort].toLowerCase().split(' ') ? -1 : 1
     })
   },
   SEARCH_FILTER(state, payload) {
